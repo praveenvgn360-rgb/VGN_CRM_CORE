e5e78eaccc1c304a57af7ce375ec9e31c7f2b92b
Praveen <praveenvgn360@gmail.com>
2026-07-16 16:14:27 +0530

    praveen active leads

---
+++ Models/ActiveLeadsAgingModel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VGN_CRM_CORE.Models
+{
+    public class ActiveLeadsAgingModel
+    {
+        public string Leadtype { get; set; }
+        public string ExecutiveId { get; set; }
+        public string TeamLeaderId { get; set; }
+        public string TeamManagerId { get; set; }
+        public string TeamHeadId { get; set; }
+        public System.Data.DataSet dsP = new System.Data.DataSet();
+
+        public DataSet dsTeam = new DataSet();
+        public DataSet dsEXE = new DataSet();
+        public DataSet dsAssistantManager = new DataSet();
+
+    }
+}
